d047535e90d7e9feb0976c0b8a969a15791e92b4
Fernando Cassio <fernandocassiodev@gmail.com>
2024-04-24 16:05:03 -0300

    transpiled code for js

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
+//IIFE to preserv code scope
 (function () {
+    //Enums
     var NotificationPlatform;
     (function (NotificationPlatform) {
         NotificationPlatform["SMS"] = "SMS";
@@ -11,9 +13,7 @@
         ViewMode["TODO"] = "TODO";
         ViewMode["REMINDER"] = "REMINDER";
     })(ViewMode || (ViewMode = {}));
-    var UUID = function () {
-        return Math.random().toString(32).substring(2, 9);
-    };
+    //Utils
     var DateUtils = {
         today: function () {
             return new Date();
@@ -25,7 +25,10 @@
         },
         formatDate: function (date) {
             return "".concat(date.getDate(), ".").concat(date.getMonth() + 1, ".").concat(date.getFullYear());
-        }
+        },
+    };
+    var UUID = function () {
+        return Math.random().toString(32).substr(2, 9);
     };
     var Reminder = /** @class */ (function () {
         function Reminder(description, date, notifications) {
@@ -34,13 +37,13 @@
             this.dateUpdated = DateUtils.today();
             this.description = "";
             this.date = DateUtils.tomorrow();
-            this.notifications = [NotificationPlatform.SMS, NotificationPlatform.EMAIL, NotificationPlatform.PUSH_NOTIFICATION];
+            this.notifications = [NotificationPlatform.EMAIL];
             this.description = description;
             this.date = date;
             this.notifications = notifications;
         }
         Reminder.prototype.render = function () {
-            return "\n      ---> Reminder <---\n      description: ".concat(this.description, "\n      date: ").concat(DateUtils.formatDate(this.date), "\n      platform: ").concat(this.notifications.join(","), "\n      ");
+            return "\n      ---> Reminder <--- \n\n      description: ".concat(this.description, " \n\n      Notify by ").concat(this.notifications.join(" and "), " in ").concat(DateUtils.formatDate(this.date), " \n\n      Created: ").concat(DateUtils.formatDate(this.date), "\n      Last Update: ").concat(DateUtils.formatDate(this.dateUpdated), "\n      ");
         };
         return Reminder;
     }());
@@ -54,12 +57,11 @@
             this.description = description;
         }
         Todo.prototype.render = function () {
-            return "\n      ---> TODO <---\n      description: ".concat(this.description, "\n      done: ").concat(this.done, "\n      ");
+            var doneLabel = this.done ? "Completed" : "In Progress";
+            return "\n      ---> TODO <--- \n\n      description: ".concat(this.description, " \n\n      Status: ").concat(doneLabel, " \n\n      Created: ").concat(DateUtils.formatDate(this.dateCreated), "\n      Last Update: ").concat(DateUtils.formatDate(this.dateUpdated), "\n      ");
         };
         return Todo;
     }());
-    var todo = new Todo("descrição");
-    var reminder = new Reminder("descrição", new Date(), [NotificationPlatform.EMAIL]);
     var taskView = {
         getTodo: function (form) {
             var todoDescription = form.todoDescription.value;
@@ -68,40 +70,44 @@
         },
         getReminder: function (form) {
             var reminderNotifications = [
-                form.notifications.value,
+                form.notification.value,
             ];
-            var reminderDate = new Date(form.reminderDate.value);
+            var reminderDate = new Date(form.scheduleDate.value);
             var reminderDescription = form.reminderDescription.value;
             form.reset();
             return new Reminder(reminderDescription, reminderDate, reminderNotifications);
         },
         render: function (tasks, mode) {
+            // Clear view
             var tasksList = document.getElementById("tasksList");
             while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
             }
+            // Render Tasks
             tasks.forEach(function (task) {
                 var li = document.createElement("li");
                 var textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
             });
+            // Render form mode
             var todoSet = document.getElementById("todoSet");
             var reminderSet = document.getElementById("reminderSet");
             if (mode === ViewMode.TODO) {
-                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("style", "display: block");
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("style", "display: block;");
                 todoSet === null || todoSet === void 0 ? void 0 : todoSet.removeAttribute("disabled");
-                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("style", "display: none");
-                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("disable", "true");
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("style", "display: none;");
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("disabled", "true");
             }
             else {
-                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("style", "display: block");
+                reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.setAttribute("style", "display: block;");
                 reminderSet === null || reminderSet === void 0 ? void 0 : reminderSet.removeAttribute("disabled");
-                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("style", "display: none");
-                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("disable", "true");
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("style", "display: none;");
+                todoSet === null || todoSet === void 0 ? void 0 : todoSet.setAttribute("disabled", "true");
             }
         },
     };
+    // Controllers
     var taskController = function (view) {
         var _a, _b;
         var tasks = [];
@@ -117,6 +123,7 @@
                     tasks.push(view.getReminder(form));
                     break;
             }
+            view.render(tasks, mode);
         };
         var handleToggleMode = function () {
             switch (mode) {
@@ -129,8 +136,10 @@
             }
             view.render(tasks, mode);
         };
-        (_a = document.getElementById("toggleMode")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", handleToggleMode);
-        (_b = document.getElementById("taskForm")) === null || _b === void 0 ? void 0 : _b.addEventListener("submit", handleEvent);
+        (_a = document
+            .getElementById("toggleMode")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", handleToggleMode);
+        (_b = document
+            .getElementById("taskForm")) === null || _b === void 0 ? void 0 : _b.addEventListener("submit", handleEvent);
     };
     taskController(taskView);
 })();
